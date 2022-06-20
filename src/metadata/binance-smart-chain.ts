bfc6cc27128c6efe945577448df690e101f6f463
Yuxuan Fan <39680169+991231@users.noreply.github.com>
2022-06-20 07:32:44 -0700

    add types and metadata

---
+++ src/metadata/binance-smart-chain.ts
@@ -0,0 +1,49 @@
+import { ChainMetadata, NetworkMetadata, SupportedNetwork } from '../types';
+
+export const BSC_CHAIN_METADATA: Readonly<ChainMetadata<'binance-smart-chain'>> = Object.freeze({
+  key: 'binance-smart-chain',
+  name: 'Binance Smart Chain',
+  logoURI: 'https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850',
+  website: 'https://binance.org/en/smartChain',
+  links: {
+    github: 'https://github.com/binance-chain/',
+    twitter: 'https://twitter.com/binance_dex',
+    reddit: 'https://reddit.com/r/BinanceExchange',
+    whitepaper: 'https://www.binance.com/resources/ico/Binance_WhitePaper_en.pdf',
+  },
+  nativeToken: {
+    key: 'binancecoin',
+    decimals: 18,
+  },
+});
+
+export const BSC_NETWORK_METADATA: Readonly<{
+  [N in SupportedNetwork['binance-smart-chain']]: NetworkMetadata<'binance-smart-chain'>;
+}> = Object.freeze({
+  mainnet: {
+    key: 'mainnet',
+    chainKey: 'binance-smart-chain',
+    name: 'Binance Smart Chain Mainnet',
+    chainId: 56,
+    isTestnet: false,
+    explorer: {
+      address: 'https://bscscan.com/address/{{id}}',
+      token: 'https://bscscan.com/token/{{id}}',
+      transaction: 'https://bscscan.com/tx/{{id}}',
+    },
+    networkId: 56,
+  },
+  testnet: {
+    key: 'testnet',
+    chainKey: 'binance-smart-chain',
+    name: 'Binance Smart Chain Testnet',
+    chainId: 97,
+    isTestnet: true,
+    explorer: {
+      address: 'https://testnet.bscscan.com/address/{{id}}',
+      token: 'https://testnet.bscscan.com/token/{{id}}',
+      transaction: 'https://testnet.bscscan.com/tx/{{id}}',
+    },
+    networkId: 97,
+  },
+});
---
+++ src/metadata/index.ts
@@ -0,0 +1,29 @@
+import { ChainMetadata, NetworkMetadata, SupportedChain, SupportedNetwork } from '../types';
+import { AVALANCHEC_CHAIN_METADATA, AVALANCHEC_NETWORK_METADATA } from './avalanche-c';
+import { BSC_CHAIN_METADATA, BSC_NETWORK_METADATA } from './binance-smart-chain';
+import { COSMOSHUB_CHAIN_METADATA, COSMOSHUB_NETWORK_METADATA } from './cosmoshub';
+import { ETHEREUM_CHAIN_METADATA, ETHEREUM_NETWORK_METADATA } from './ethereum';
+import { POLYGON_CHAIN_METADATA, POLYGON_NETWORK_METADATA } from './polygon';
+import { SOLANA_CHAIN_METADATA, SOLANA_NETWORK_METADATA } from './solana';
+
+export const CHAIN_REGISTRY: Readonly<{
+  [C in SupportedChain]: ChainMetadata<C>;
+}> = Object.freeze({
+  'avalanche-c': AVALANCHEC_CHAIN_METADATA,
+  'binance-smart-chain': BSC_CHAIN_METADATA,
+  cosmoshub: COSMOSHUB_CHAIN_METADATA,
+  ethereum: ETHEREUM_CHAIN_METADATA,
+  polygon: POLYGON_CHAIN_METADATA,
+  solana: SOLANA_CHAIN_METADATA,
+});
+
+export const NETWORK_REGISTRY: Readonly<{
+  [C in SupportedChain]: { [N in SupportedNetwork[C]]: NetworkMetadata<C> };
+}> = Object.freeze({
+  'avalanche-c': AVALANCHEC_NETWORK_METADATA,
+  'binance-smart-chain': BSC_NETWORK_METADATA,
+  cosmoshub: COSMOSHUB_NETWORK_METADATA,
+  ethereum: ETHEREUM_NETWORK_METADATA,
+  polygon: POLYGON_NETWORK_METADATA,
+  solana: SOLANA_NETWORK_METADATA,
+});
